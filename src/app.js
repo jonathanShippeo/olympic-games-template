d721d1affc57d65e1ebc22d88da7a763b0061208
cbouvard <christophe.bouvard@gmail.com>
2021-12-09 14:35:51 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+// Ajouter les routes via les routers
+const sportRouter = require('./routers/sport.router');
+app.use('/api', sportRouter);
+// ... A COMPLETER ...
+
+// Connexion à la base de données
+require('./database/mongodb');
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serveur lancé sur le port ${port} 🚀`);
+});
+
