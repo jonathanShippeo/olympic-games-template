8aa404ebfa288430dcdf2b93b7b30d136e6ad08c
Ahmet YILMAZ <a.yilmaz@nicomatic.fr>
2021-12-09 16:40:54 +0100

    add database connection and dotenv

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const uri = process.env.MONGODB_URL;
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 
