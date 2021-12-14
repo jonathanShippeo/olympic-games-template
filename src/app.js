0fcd1dbd258ea00adb27d4914581499164576631
jonathanShippeo <83287999+jonathanShippeo@users.noreply.github.com>
2021-12-14 20:42:41 +0100

    Ajout page accueil, views et morgan

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
+<<<<<<< Updated upstream
 const express = require('express');
+=======
+// Connexion à la base de données
+>>>>>>> Stashed changes
 require('./database/mongodb');
 
+//Variables / bibliothèques
+const express = require('express');
 const app = express();
+const path = require('path');
+const logger = require('morgan');
 
+app.use(logger('dev'));
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: false }));
 // Ajouter les routes via les routers
+//const sportRouter = require('./routers/sport.router');
+
+//app.use('/api', sportRouter);
+// ... A COMPLETER ...
+
+app.set('view engine', 'hbs');
+// on indique que nos vues se trouverons toujours dans le dossier views 
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('index', { name: 'TweetJS' });
+});
+ 
+
+
+
+app.post('/tweets', (req, res) => {
+    const body = req.body;
+    tweets.push(body);
+    res.redirect('/tweets');
+});
+
 const sportRouter = require('./routers/sport.router');
 app.use('/api/sports', sportRouter);
 
