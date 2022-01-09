a4d3de0b5a36dbe92329117bce84c496c6b7800d
Ahmet YILMAZ <a.yilmaz@nicomatic.fr>
2022-01-09 07:52:31 +0100

    Transfert business logic from router to controller

--- src/controllers/athlete.controller.js
+++ src/controllers/athlete.controller.js
@@ -1,68 +1,96 @@
-const Athlete = require('../models/athlete.model');
-const Sport = require('../models/sport.model');
-let mongoose = require('mongoose');
+const Athlete = require("../models/athlete.model");
+const Sport = require("../models/sport.model");
+let mongoose = require("mongoose");
+const fetch = require("node-fetch");
 
 class AthleteController {
-    /*List athletes*/
-    async list(req, res) {
-        const athlete = await Athlete.find();
-        return athlete
-    }
 
-    async getNameAthleteById(athleteId ,res){
-        const AthleteById = await Athlete.findById(athleteId);
-        const AthleteName = AthleteById.firstName + " " + AthleteById.lastName
-        return AthleteName;
-    }
-
-    async getAthleteById(athleteId ,res){
-      const athlete = await Athlete.findById(athleteId);
-      //console.log(athlete);
-      return athlete
+  /*API List athletes*/
+  async list(req, res) {
+    const athletes = await Athlete.find();
+    res.json(athletes);
   }
 
-    async insertAthlete(formAthlete, res){
-       
-       try {
-            if(formAthlete.firstName == "" && formAthlete.lastName == "" && formAthlete.gender == "" && formAthlete.country == "" ){
-                console.log("Le champs est vide, veuillez remplir une valeur")
-                res.redirect('/api/athletes')
-            } else {
-                //ajouter dans collection mongoose
-                Athlete.create(formAthlete);
-                //redirection sur lui-même
-                res.redirect('/api/athletes')
-            }
-        
-        } catch (error) {
-            console.log("Le champs n'est pas valide")
-            res.redirect('/api/athletes')
-        }
-        
-    }
+  /*API List one athlete*/
+  async getAthleteById(athleteId, res) {
+    const athlete = await Athlete.findById(athleteId);
+    res.json(athlete);
+  }
 
-    async listSportsByAthlete(athleteId, res) {
-        const listSportsAthlete = await Sport.find({ 'athletes': {$in : mongoose.Types.ObjectId(athleteId) }});
-        return listSportsAthlete;
-    }
+  /*API Lists sport of an athlete*/
 
-    //supprimer un candidat
-    async deleteAthlete(athleteId, res) {
-        Athlete.findByIdAndRemove(athleteId, (err, selectedAthlete) => {
-          if (err) return res.status(500).send(err);
-    
-          const response = {
-            message: "Athlete successfully deleted",
-            Deleted_id: selectedAthlete._id,
-          };
-          return res.status(200).send(response);
-      });
-    }  
-    
-    
-    /**  TODO: modify
-   * Endpoint to edit a Athlete with a spesific ID
+  async listSportsForOne(req, res) {
+    const idAthlete = req.params.athleteId;
+    const sportsForThisAthlete = await Sport.find({
+      athletes: { $all: [idAthlete] },
+    });
+
+    const sports = sportsForThisAthlete.map((x) => {
+      const name = x.name;
+      const _data = name;
+      return _data;
+    });
+
+     return sports;
+  }
+
+   /**
+   * Create a new athlete  {POST}
+   */
+    async create(req, res) {
+      if (!req.body || !req.body.firstName) {
+        console.error("ooops ajoutez tous les champs surout prenom");
+        res.status(400).end();
+        return;
+      }
   
+      const newAthlete = new Athlete(req.body);
+      newAthlete
+        .save()
+        .then((addedItem) => {
+          console.log("New Athlete added");
+          return res.status(200).send(addedItem);
+        })
+        .catch((err) => {
+          if (err) return res.status(500).send(err);
+        });
+    }
+
+  async listSportsByAthlete(athleteId, res) {
+    const listSportsAthlete = await Sport.find({
+      athletes: { $in: mongoose.Types.ObjectId(athleteId) },
+    });
+    return listSportsAthlete;
+  }
+
+
+
+  async getNameAthleteById(athleteId, res) {
+    const AthleteById = await Athlete.findById(athleteId);
+    const AthleteName = AthleteById.firstName + " " + AthleteById.lastName;
+    return AthleteName;
+  }
+
+  async insertAthlete(formAthlete, res) {
+    try {
+      if (
+        formAthlete.firstName == "" &&
+        formAthlete.lastName == "" &&
+        formAthlete.gender == "" &&
+        formAthlete.country == ""
+      ) {
+        console.log("Le champs est vide, veuillez remplir une valeur");
+        res.redirect("/athletes");
+      } else {
+        Athlete.create(formAthlete);
+        res.redirect("/athletes");
+      }
+    } catch (error) {
+      console.log("Le champs n'est pas valide");
+      res.redirect("/athletes");
+    }
+  }
+
   async delete(req, res) {
     // The "selectedAthlete" in this callback function represents the document that was found.
     Athlete.findByIdAndRemove(req.params.id, (err, selectedAthlete) => {
@@ -76,7 +104,29 @@ class AthleteController {
       return res.status(200).send(response);
     });
   }
-   */
+
+  //API delete an athlete => supprimer un candidat
+  async deleteAthlete(athleteId, res) {
+    console.log(athleteId);
+    Athlete.findByIdAndRemove(athleteId, (err, selectedAthlete) => {
+      if (err) return res.status(500).send(err);
+      res.redirect("/athletes");
+    });
+  }
+
+  //Page Operations
+
+  async athletes(req, res) {
+    console.log("INFO: -> Page url: " + req.url);
+    const listAthlete = await Athlete.find();
+    res.render("athletes", { listAthlete, main: true });
+  }
+
+  async athleteDetails(athleteId, res) {
+    const athlete = await Athlete.findById(athleteId);
+    //console.log(id);
+    res.render("athletes", { athlete,athleteId, main: false });
+  }
 }
 
 module.exports = AthleteController;
--- src/controllers/sport.controller.js
+++ src/controllers/sport.controller.js
@@ -1,68 +1,214 @@
-const Athlete = require('../models/athlete.model');
-const Sport = require('../models/sport.model');
-const mongoose = require('mongoose');
+const Athlete = require("../models/athlete.model");
+const Sport = require("../models/sport.model");
+const mongoose = require("mongoose");
+const res = require("express/lib/response");
 
 class SportController {
-    /*List sports*/
-    async list(req, res) {
-      const sports = await Sport.find();
-      return sports
+  /**
+   * API: List sports {GET}
+  */
+  async list(req, res) {
+    const sports = await Sport.find();
+    res.json(sports);
   }
 
-    async getNameSportById(sportId ,res){
-        const SportById = await Sport.findById(sportId);
-        return SportById.name;
-    }
+  /**
+   * API: Athletes for a spesific Sport  {GET}
+   */
+  async getAthletesForSport(sportId,res){
+    const selectedSport = await Sport.findById(sportId)
+     res.json(selectedSport.athletes);
+  }
 
-  async getSportById(sportId ,res){
-    const sport = await Sport.findById(sportId);
-    return sport
+  /**
+    * API: Create a new sport  {POST}
+  */
+     async create(req, res) {
+
+      const sport= req.body.name;
+      const idAthletes= req.body.athletes;
+
+     const newSport = new Sport({name: sport , athletes: idAthletes});
+     newSport.save()
+     .then((addedItem) =>{
+         console.log('New Sport added')
+         return res.status(200).send(addedItem);} )
+     .catch(err => {
+         if (err) return res.status(500).send(err);
+     })
+ }
+
+
+
+  async listAthleteIdBySport(sportId, res) {
+    const SportById = await Sport.findById(sportId);
+    return SportById.athletes;
+  }
+
+
+/**
+   * API: add an athlete to a spesific sport 
+   *
+   */
+ async addOneAthleteToSport(idSport, athleteId, res) {
+  // Query to get content of selected sport
+  const selectedSport = await Sport.findById(idSport);
+  console.log(selectedSport.athletes);
+
+  //Prepare body to add
+  let athletesToPost = [];
+  athletesToPost = selectedSport.athletes;
+
+  if (!athletesToPost.includes(athleteId)) {
+    athletesToPost.push(athleteId);
+  }
+  //modify by update
+  const filter = { _id: idSport };
+  const update = { athletes: athletesToPost };
+
+  Sport.findByIdAndUpdate(
+    filter,
+    update,
+    { new: true },
+    (err, updatedItem) => {
+      // Handle any possible database errors
+      if (err) return res.status(500).send(err);
+      return res.send(updatedItem);
+    }
+  );
+  //TODO Error handling for this end point
 }
 
 
-    async insertSport(sportName, res){
-        //add collection mongoose
-        Sport.create(sportName);
-        //redirection
-        res.redirect('/api/sports')
-    }
+/**  API: modify  
+     * Endpoint to edit a sport with a spesific ID
+     */
+ async modify(req, res) {
 
-    
-    async listAthleteIdBySport(sportId ,res){
-        const SportById = await Sport.findById(sportId);
-        return SportById.athletes;
-    }
+  const idSport = req.body.id;
+  const sport= req.body.name;
+  const idAthletes= req.body.athletes;
+
+  const filter = {_id:idSport}
+  const update ={name:sport , athletes:idAthletes}
+
+ Sport.findByIdAndUpdate(filter,update,{new: true},
+     (err, updatedItem) => {
+     // Handle any possible database errors
+         if (err) return res.status(500).send(err);
+         return res.send(updatedItem);
+     }
+ )
+
+}
+
+/**  API Delete 
+  * Endpoint to delete a sport with a spesific ID
+  */
+ async delete(req, res) {
+  // The "selectedSport" in this callback function represents the document that was found.
+  Sport.findByIdAndRemove(req.params.id,(err,selectedSport)=>{
+  if (err) return res.status(500).send(err);
+
+  const response = {
+      message: "Sport successfully deleted",
+      Deleted_id: selectedSport._id
+  };
+
+  return res.status(200).send(response);
+
+ });
+
+}
+
 
  
 
-    //Ajouter Athlete dans un sport
-    async addAthleteInSport(sportId, athleteId){
-        const oSport = await Sport.findById(sportId);
-        const oAthlete = athleteId;
-        oSport.athletes.push(mongoose.Types.ObjectId(oAthlete));
-        oSport.save();
+  //supprimer un sport par front page
+  async deleteSport(sportId, res) {
+    Sport.findByIdAndRemove(sportId, (err, selectedSport) => {
+      if (err) return res.status(500).send(err);
+       res.redirect("/sports")
+    });
+  }
+
+  //Page Operations
+
+  async sports(req, res) {
+    const listSport = await Sport.find();
+    res.render("sports", { listSport, main: true });
+  }
+
+  //Liste athlètes par sports
+  async sportDetails(sportId, res) {
+    const sportName = await this.getNameSportById(sportId);
+    const sport = await this.getSportById(sportId);
+    const listAthleteIdBySport = await this.listAthleteIdBySport(sportId, res);
+    const listAthleteBySport = await Athlete.find({
+      _id: { $in: listAthleteIdBySport },
+    });
+    const listAthlete = await Athlete.find();
+    return res.render("sports", {
+      listAthleteBySport,
+      listAthlete,
+      sportId,
+      sportName: sportName,
+      sport,
+      main: false,
+    });
+  }
+
+  
+  async getNameSportById(sportId, res) {
+    const SportById = await Sport.findById(sportId);
+    return SportById.name;
+  }
+
+  async getSportById(sportId, res) {
+    const sport = await Sport.findById(sportId);
+    return sport;
+  }
+
+  async insertSport(sportName, res) {
+    Sport.create(sportName);
+    res.redirect("/sports");
+  }
+
+   //Ajouter Athlete dans un sport
+   async addAthleteInSport(sportId, athleteId) {
+    const oSport = await Sport.findById(sportId);
+    const oAthlete = athleteId;
+    oSport.athletes.push(mongoose.Types.ObjectId(oAthlete));
+    oSport.save();
+  }
+
+  async deleteAthleteFromSport(sportId, athleteId, res) {
+    const sport = await this.getSportById(sportId);
+
+    let athletesToUpdate = sport.athletes;
+
+    //Prepare body to add
+    for (let i = 0; i < athletesToUpdate.length; i++) {
+      if (athletesToUpdate[i] == athleteId) {
+        athletesToUpdate.splice(i, 1);
+      }
     }
 
-    //supprimer un sport
-    async deleteSport(sportId, res) {
-      Sport.findByIdAndRemove(sportId, (err, selectedSport) => {
+    const filter = { _id: sportId };
+    const update = { athletes: athletesToUpdate };
+    Sport.findByIdAndUpdate(
+      filter,
+      update,
+      { new: true },
+      (err, updatedItem) => {
+        // Handle any possible database errors
         if (err) return res.status(500).send(err);
-  
-        const response = {
-          message: "Sport successfully deleted",
-          Deleted_id: selectedSport._id,
-        };
-        return res.status(200).send(response);
-    });
-  }  
+        res.status(200);
+        console.log('*************************Athlete deleted from this sport *********************');
+      }
+    );
 
-  /**  TODO: modify
-   * Endpoint to edit a sport with a spesific ID
-   
-  
-
-    
-*/
+  }
 }
 
 module.exports = SportController;
