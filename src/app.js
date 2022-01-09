a8515caa1b7e8a63089cbfb45962f98f16ab7f23
Ahmet YILMAZ <a.yilmaz@nicomatic.fr>
2022-01-09 07:49:20 +0100

    Update new routers and arboressance doc

--- src/app.js
+++ src/app.js
@@ -1,27 +1,32 @@
 const express = require('express');
-const bodyParser= require('body-parser')
 const path = require('path');
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }));
+
+const sportApiRouter = require('./routers/api/sport.api.router');
+const athleteApiRouter = require('./routers/api/athlete.api.router');
+
+const athletesPageRouter = require('./routers/front/athletes.page.router');
+const sportsPageRouter = require('./routers/front/sports.page.router');
+
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-// on supprime la ligne qui importait notre fichier json et celle du uuid ! Mongodb génére automatiquement les ids
-//const { v4: uuidv4 } = require('uuid'); // pour générer un id : uuidv4();  ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
-const sportRouter = require('./routers/sport.router');
-app.use('/api/sports', sportRouter);
 
-const athleteRouter = require('./routers/athlete.router');
-app.use('/api/athletes', athleteRouter);
+app.use('/api/sports', sportApiRouter);
+app.use('/api/athletes', athleteApiRouter);
 
+//VIEWS Front end
 app.get('/', (req, res) => {
     res.render('index', { name: 'CyberGames' });
 });
 
-app.get('/index', (req, res) => {
-    res.render('index', { name: 'CyberGames' });
-});
+//athletes pages
+app.use('/athletes',athletesPageRouter);
+//sports pages
+app.use('/sports',sportsPageRouter);
 
 // Connexion à la base de données
 require('./database/mongodb');
